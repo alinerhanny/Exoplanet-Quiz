6caebe736238df046baf94149284c92bcddc5ee4
rhanny <alinerhanny@gmail.com>
2024-10-25 20:57:56 -0300

    Refactor: estrutura da lista de perguntas

--- quiz/questions.js
+++ quiz/questions.js
@@ -2,80 +2,89 @@ let questions = [
   {
     numb: 1,
     question: "What is an exoplanet?",
-    options: ["A planet that orbits the Sun", 
-              "A planet that orbits a star outside our solar system",
-              "A planet that orbits a black hole",
-              "A planet that orbits the Moon"],
-    correctAnswer: "A planet that orbits a star outside our solar system"
+    options: [
+      { id: 1, text: "A planet that orbits the Sun"},
+      { id: 2, text:  "A planet that orbits a star outside our solar system"},
+      { id: 3, text: "A planet that orbits a black hole"},
+      { id: 4, text: "A planet that orbits the Moon"}       
+    ],
+    correctAnswerId: 2
   },
   {
     numb: 2,
     question: "Exoplanets can be made of different materials. Which of the following statements is true?",
-    options: ["All exoplanets are made of gas", 
-              " All exoplanets are rocky", 
-              "Some exoplanets are gaseous, others are rocky",
-              "All exoplanets are just like Earth",
-              ],
-    correctAnswer: "Some exoplanets are gaseous, others are rocky"
+    options: [
+      { id: 1, text: "All exoplanets are made of gas"},
+      { id: 2, text:  "All exoplanets are rocky"},
+      { id: 3, text: "Some exoplanets are gaseous, others are rocky"},
+      { id: 4, text: "All exoplanets are just like Earth"}       
+    ],
+    correctAnswerId: 3
   },
   {
     numb: 3,
     question: "How many exoplanets have been confirmed so far?",
-    options: ["More than 1,000", 
-              "More than 3,000", 
-              "More than 5,000",
-              "More than 10,000",
-              ],
-    correctAnswer: "More than 5,000"
+    options: [
+      { id: 1, text: "Approximately 1,000"},
+      { id: 2, text: "Approximately 3,000"},
+      { id: 3, text: "More than 5,000"},
+      { id: 4, text: "More than 10,000"}       
+    ],
+    correctAnswerId: 3
   },
   {
     numb: 4,
     question: "Which of these extreme conditions can be found on some exoplanets?",
-    options: ["Cold climates with eternal snow", 
-              "Acid storms and suffocating heat", 
-              "Diamond rains",
-              "Atmospheres filled with oxygen",
-              ],
-    correctAnswer: "Acid storms and suffocating heat"
+    options: [
+      { id: 1, text: "Cold climates with eternal snow"},
+      { id: 2, text: "Acid storms and suffocating heat"},
+      { id: 3, text: "Diamond rains"},
+      { id: 4, text: "Atmospheres filled with oxygen"}       
+    ],
+    correctAnswerId: 2
   },
   {
     numb: 5,
     question: "What is the most common method for discovering exoplanets?",
-    options: ["Observing black holes", 
-              "Detecting solar flares", 
-              "Directly photographing the planets",
-              " Observing the transit of a planet in front of a star ",
-              ],
-    correctAnswer: " Observing the transit of a planet in front of a star "
+    options: [
+      { id: 1, text: "Observing black holes"},
+      { id: 2, text: "Detecting solar flares"},
+      { id: 3, text: "Directly photographing the planets"},
+      { id: 4, text: "Observing the transit of a planet in front of a star"}       
+    ],
+    correctAnswerId: 4
   },
   {
     numb: 6,
     question: "What percentage of discovered exoplanets are in the habitable zone?",
-    options: ["Around 10%", 
-              "Around 20%", 
-              "Around 50%",
-              "Around 75%",
-              ],
-    correctAnswer: "Around 20%"
+    options: [
+      { id: 1, text: "Around 10%"},
+      { id: 2, text: "Around 20%"},
+      { id: 3, text: "Around 50%"},
+      { id: 4, text: "Around 75%"}       
+    ],
+    correctAnswerId: 2
   },
   {
     numb: 7,
     question: "What are exoplanets that orbit more than one star called?",
-    options: ["Binary planets", 
-              "Twin planets", 
-              "Circumbinary planets",
-              "Double planets",
-              ],
-    correctAnswer: "Circumbinary planets"
+    options: [
+      { id: 1, text: "Binary planets"},
+      { id: 2, text: "Twin planets"},
+      { id: 3, text: "Circumbinary planets"},
+      { id: 4, text: "Double planets"}       
+    ],
+    correctAnswerId: 3
   },
   {
     numb: 8,
     question: "Which telescope is mentioned as important for studying exoplanets?",
-    options: ["James Webb", 
-              "Kepler", 
-              "Hubble",
-              "Spitzer",
-              ],
-    correctAnswer: "James Webb"
+    options: [
+      { id: 1, text: "James Webb"},
+      { id: 2, text: "Kepler"},
+      { id: 3, text: "Hubble"},
+      { id: 4, text: "Spitzer"}       
+    ],
+    correctAnswerId: 1
   },
 ]
